aa5b00a15796296766fdd16d3e1190b016883876
Paulo Bardes <paulobardes@ig.com.br>
2015-09-12 02:02:10 -0300

    Prototipo funcionando

--- color.py
+++ color.py
@@ -1,28 +1,31 @@
-#/usr/bin/python
+#!/usr/bin/python3
 
 # Evita "stack overflow"
 import sys
 sys.setrecursionlimit(1100)
 
-COLORS = frozenset(('Azul', 'Verde', 'Vermelho', 'Amarelo'))
+COLORS = frozenset({'Azul', 'Verde', 'Vermelho', 'Amarelo'})
 
 class Node:
-    def __init__(self, name, color):
-        if(type(name) != str)
+    def __init__(self, name, color=None):
+        if not isinstance(name, str):
             raise TypeError("Node name must be a string.")
         self.Name = name;
-        self.Color = None
+        self.Color = color
 
     def __hash__(self):
-        return hash(self.Name)
+        return hash((self.Name, "NODE"))
 
     def __eq__(self, to):
-        if type(to) == str:
-            return self.Name == to
-        elif type(to) == type(self):
+        if isinstance(to, self.__class__):
             return self.Name == to.Name
+        elif isinstance(to, str):
+            return self.Name == to
         else:
             return False
+
+    def __str__(self):
+        return "<Node: {}, {}>".format(self.Name, self.Color or "[em branco]")
 
     def SetColor(self, color):
         if color in COLORS or color == None:
@@ -33,65 +36,69 @@ class Node:
     def ClearColor(self):
         self.Color = None
 
-    def GetColor(self)
+    def GetColor(self):
         return self.Color
-        
+
 
 class Graph:
     def __init__(self):
         self.Nodes = {}
 
     def __getitem__(self, name):
-        if type(name) != str:
+        if not isinstance(name, str):
             raise TypeError("Node name must be a string.")
-        return self.Nodes[name]
+        return self.Nodes[name][0]
+
+    def __str__(self):
+        return str(self.Nodes)
 
     def AddNode(self, name):
-        if type(name) != str:
+        if not isinstance(name, str):
             raise TypeError("Node name must be a string.")
-        
+
         if name in self.Nodes:
             return True
         else:
-            self.Nodes[Node(name)] = set()
+            self.Nodes[name] = (name, set())
 
     def Connect(self, node1, node2):
-        if type(node1) != str or type(node2) != str:
+        if not isinstance(node1, str) or not isinstance(node2, str):
             raise TypeError("Node names must be strings.")
-
         self.AddNode(node1)
         self.AddNode(node2)
-        self.Nodes[node1].add(node2)
-        self.Nodes[node2].add(node1)
+        self.Nodes[node1][1].add(node2)
+        self.Nodes[node2][1].add(node1)
 
-    def GetNeighbors(self, node):
-        if(type(node) != str)
+    def GetNeighbors(self, name):
+        if not isinstance(name, str):
             raise TypeError("Node name must be a string.")
 
-        if node not in self.Nodes:
+        if name not in self.Nodes:
             return None
-        return frozenset(self.Nodes[node])
+        return frozenset(self.Nodes[name][1])
 
     def LoadFromFile(self, f):
         for line in f:
             s = line.split()
-            self.Nodes[s[:-1]] = {name[:-1] for name in s}
+            node_name = s[0][:-1]
+            self.Nodes[node_name] = (Node(node_name),
+                                     set([name[:-1] for name in s[1:]]))
 
 def GetNextNode(nodes):
-    for n in nodes:
-        if n.Color == None:
-            return n.Name
+    for n in nodes.Nodes:
+        if nodes[n].Color == None:
+            return n
     return None
 
 def GetColors(nodes, target):
     available = set(COLORS)
-    
+
     for n in nodes.GetNeighbors(target):
-        available.discard(n.Color)
+        available.discard(nodes[n].Color)
 
     return available
 
-def SolveNextColor(nodes)
+def SolveNextColor(nodes):
     # Determina o próximo nó a ser aprofundado
     target = GetNextNode(nodes)
 
@@ -103,7 +110,7 @@ def SolveNextColor(nodes)
     for color in GetColors(nodes, target):
         nodes[target].SetColor(color) # Atribui a cor
         nodes = SolveNextColor(nodes) # Tenta aprofundar ainda mais
-        
+
         if not nodes:
             # Se não conseguiu aprofundar troca de cor
             nodes[target].ClearColor()
@@ -114,4 +121,13 @@ def SolveNextColor(nodes)
     # Se nenhuma cor levou a uma solução, falha.
     return None
 
-print("Hello :)")
+nodes = Graph()
+f = open('in')
+_type = f.readline().split()[1]
+nodes.LoadFromFile(f)
+f.close()
+
+SolveNextColor(nodes)
+
+for n in nodes.Nodes:
+    print("{}: {}".format(nodes[n].Name, nodes[n].Color))
