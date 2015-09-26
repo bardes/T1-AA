13c43c8918ae36f0bf58b6f968e42e3d5018e765
Paulo Bardes <paulobardes@ig.com.br>
2015-09-26 16:50:35 -0300

    Terminado backtracking simples.

--- color.py
+++ color.py
@@ -12,6 +12,7 @@ class Node:
             raise TypeError("Node name must be a string.")
         self.Name = name;
         self.Color = color
+        self.AvailableColors = set(COLORS)
 
     def __hash__(self):
         return hash((self.Name, "NODE"))
@@ -45,12 +46,15 @@ class Graph:
         self.Nodes = {}
 
     def __getitem__(self, name):
-        if not isinstance(name, str):
-            raise TypeError("Node name must be a string.")
         return self.Nodes[name][0]
 
     def __str__(self):
         return str(self.Nodes)
+
+    def __iter__(self):
+        for node in self.Nodes.values():
+            yield node[0]
+        raise StopIteration
 
     def AddNode(self, name):
         if not isinstance(name, str):
@@ -64,10 +68,15 @@ class Graph:
     def Connect(self, node1, node2):
         if not isinstance(node1, str) or not isinstance(node2, str):
             raise TypeError("Node names must be strings.")
+        elif (self[node1].Color == self[node2].Color) and self[node1].Color:
+            raise Exception("Cannot connect nodes of the same color!")
+
         self.AddNode(node1)
         self.AddNode(node2)
         self.Nodes[node1][1].add(node2)
         self.Nodes[node2][1].add(node1)
+        self.CalcAvailableColors(node1)
+        self.CalcAvailableColors(node2)
 
     def GetNeighbors(self, name):
         if not isinstance(name, str):
@@ -77,24 +86,58 @@ class Graph:
             return None
         return frozenset(self.Nodes[name][1])
 
+    def Paint(self, target, color):
+        if color not in COLORS:
+            raise TypeError("Invalid color: \"{}\"".format(color))
+        elif color not in self[target].AvailableColors:
+            return False
+
+        if(self[target].Color != None):
+            self.Unpaint(target)
+
+        self[target].SetColor(color)
+        for node in self.GetNeighbors(target):
+            self[node].AvailableColors.discard(color)
+
+        return True
+
+    def CalcAvailableColors(self, target):
+        self[target].AvailableColors = set(COLORS)
+        for n in self.GetNeighbors(target):
+            self[target].AvailableColors.discard(self[n].Color)
+
+    def Unpaint(self, target):
+        self[target].Color = None
+        for n in self.GetNeighbors(target):
+            self.CalcAvailableColors(n)
+
     def LoadFromFile(self, f):
         for line in f:
             s = line[:-1].split(':')
             self.Nodes[s[0]] = (Node(s[0]), set(s[1].split(',')))
 
+    def ValidadteSymmetry(self):
+        flag = True
+        for node in self.Nodes:
+            for neighbor in self.GetNeighbors(node):
+                if neighbor not in self.Nodes:
+                    print("Warning: non existent node \"{}\"" \
+                            .format(neighbor), file=sys.stderr)
+                    flag = False
+                elif node not in self.GetNeighbors(neighbor):
+                    print("Warning: non symmetric edge from {} to {}" \
+                            .format(node, neighbor), file=sys.stderr)
+                    flag = False
+        return flag
+
 def GetNextNode(nodes):
-    for n in nodes.Nodes:
-        if nodes[n].Color == None:
+    for n in nodes:
+        if n.Color == None:
             return n
     return None
 
 def GetColors(nodes, target):
-    available = set(COLORS)
-
-    for n in nodes.GetNeighbors(target):
-        available.discard(nodes[n].Color)
-
-    return available
+    return frozenset(nodes[target].AvailableColors)
 
 def SolveNextColor(nodes):
     # Determina o próximo nó a ser aprofundado
@@ -105,25 +148,34 @@ def SolveNextColor(nodes):
         return True;
 
     # Pra cada possível solução
-    for color in GetColors(nodes, target):
-        nodes[target].SetColor(color) # Atribui a cor
-        if not SolveNextColor(nodes): # Tenta aprofundar ainda mais
-            # Se não conseguiu aprofundar troca de cor
-            nodes[target].ClearColor()
-        else:
-            # Se achou uma solução retorna
-            return True
+    for color in COLORS:
+        if nodes.Paint(target.Name, color): # Tenta pintar o nó
+            if not SolveNextColor(nodes):   # Tenta aprofundar ainda mais
+                nodes.Unpaint(target.Name)  # Se não conseguiu desfaz a pintura
+            else:
+                return True # Se achou uma solução retorna True
 
     # Se nenhuma cor levou a uma solução, falha.
     return False
 
-nodes = Graph()
-infile = sys.stdin
-infile.readline().split()[1]
-nodes.LoadFromFile(infile)
-infile.close()
+if __name__ == '__main__':
+    nodes = Graph() # Cria um grafo vazio
 
-SolveNextColor(nodes)
+    # Tenta abrir o arquivo dado e pular a primeira linha
+    infile = open(sys.argv[1])
+    infile.readline()
 
-for n in nodes.Nodes:
-    print("{}: {}".format(nodes[n].Name, nodes[n].Color))
+    # Carrega os dados e fecha o arquivo
+    nodes.LoadFromFile(infile)
+    infile.close()
+
+    # verifica se grafo lido é válido
+    if(nodes.ValidadteSymmetry() == False):
+        sys.exit(1) # Se não for sai com erro
+
+    # Começa o backtracking
+    SolveNextColor(nodes)
+
+    # Imprime os resultados
+    for n in nodes:
+        print("{}: {}".format(n.Name, n.Color))
